fa69976a1295621121fb326031f021b7856f3da4
Eduardo Mesquita <kadubezas@gmail.com>
2022-02-19 14:08:49 -0300

    total, subtotal metods

--- src/main/java/com/kadubezas/course/entities/OrderItem.java
+++ src/main/java/com/kadubezas/course/entities/OrderItem.java
@@ -12,17 +12,17 @@ import com.kadubezas.course.entities.pk.OrderItemPk;
 
 @Entity
 @Table(name = "tb_order_item")
-public class OrderItem implements Serializable{
+public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPk id = new OrderItemPk();
-	
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
-		
+
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
@@ -32,24 +32,24 @@ public class OrderItem implements Serializable{
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
-	
+
 	public Integer getQuantity() {
 		return quantity;
 	}
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable{
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return quantity * price;
 	}
 
 	@Override
